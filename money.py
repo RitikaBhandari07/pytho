7cfec7ee233467d3e9958f325638aab2998f4e35
RitikaBhandari07 <167596764+RitikaBhandari07@users.noreply.github.com>
2024-04-28 18:37:00 +0530

    Add files via upload

---
+++ money.py
@@ -0,0 +1,14 @@
+rupees=288
+a=rupees//200
+print("200 rupees note are ", a)
+
+remainder1=rupees%200
+print("remaining balance is",remainder1)
+
+b=remainder1//50
+print("50 rupees notes are",b)
+
+remainder2=rupees%50
+print("remaining balance is ",remainder2)
+
+# floor division gives the integer value.
