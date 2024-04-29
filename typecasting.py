884ad31304e666f75012dd2e98c5e6fefab43fcd
RitikaBhandari07 <167596764+RitikaBhandari07@users.noreply.github.com>
2024-04-29 21:22:06 +0530

    Add files via upload

---
+++ typecasting.py
@@ -0,0 +1,16 @@
+# # conversion of any datatype to integer,float or any datatype.
+# a= int(1)
+# b=int(23.5)
+# c=int("10")
+# d=float(10)
+# e=float("10")
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e) 
+
+
+a="my name is don"
+print(a[11])
+print(a[5])
