8737b322ccba2a4d9c79c6025e29cdd93578e18c
RitikaBhandari07 <167596764+RitikaBhandari07@users.noreply.github.com>
2024-05-01 14:28:37 +0530

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,19 @@
+# list is collection of all possible elements and is mutable any datatype can be added but must be relevant
+# []->subscript.
+# in python interpreter will stop at one place earlier , eg:-if you have index 10 it will stop at 9 , if we traverse in forward direction 
+
+cricketers =["dhoni","kohli","dinesh","pandya","gill","rahane","gaikwad","yadav","pant","rahul","karthik"]
+print(cricketers)
+print(cricketers[0:10:1])
+print(cricketers[0:11:1])
+print(cricketers[0:11:2])
+print(cricketers[::])
+print(cricketers[::-1])
+print(cricketers[10:0:-1])
+print(type(cricketers))
+print(cricketers[1])
+print(cricketers[10])
+print(cricketers[-1])
+print(cricketers[-1:-8:-1])
+print(cricketers[:5])
+
