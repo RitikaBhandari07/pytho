a48852380e64cc355af517d9b26de9cc37790881
RitikaBhandari07 <167596764+RitikaBhandari07@users.noreply.github.com>
2024-05-03 10:33:26 +0530

    Add files via upload

---
+++ list4.py
@@ -0,0 +1,19 @@
+mylist=["ritika","ayan","gandhali","mobile","laptop"]
+# mylist[1:2]=["iphone","pen"]
+# print(mylist)
+# mylist[2:3]=["iphone","pen"]
+# print(mylist)
+# mylist[1:3]=["iphone","pen"]
+# print(mylist)
+# mylist[1:2]=["iphone","pen"]
+# print(mylist)
+# mylist[0:4]=["iphone","pen"]
+# print(mylist)
+
+# mylist[1]="sachin"
+# print(mylist)
+
+if "ritika" in mylist:
+    print("yes,'ritika'is in the list")
+else:
+    print("no,'ritika'is not in the list")
