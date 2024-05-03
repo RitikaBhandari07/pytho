316a148db5905ab7ce91a205f6cc48b8d12a89d9
RitikaBhandari07 <167596764+RitikaBhandari07@users.noreply.github.com>
2024-05-03 22:36:02 +0530

    Add files via upload

---
+++ loops1.py
@@ -0,0 +1,49 @@
+mylist=["ritika","ayan","gandhali","mobile","laptop"]
+
+for i in mylist:
+    print(i)
+
+for i in "bottle":
+    print(i)
+
+for i in mylist:
+    print (i)
+
+    if i=="mobile":
+        break
+
+
+for i in mylist:
+    print (i)
+
+    if i=="mobile":
+        continue
+
+
+
+for i in range(10):
+    print(i)
+
+for i in range (5,51,5):
+    print(i)
+
+for i in range(6):
+    if i==3:
+
+        break
+    print(i)
+else:
+
+    print("goodbyeeeee")
+
+
+
+
+
+special_name=["clever","good","beautiful","smart"]
+name=["ritika","ayan","gandhali","sachin"]
+for i in special_name:
+     for j in name:
+          print(i,j)
+
+
