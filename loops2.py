7a071ab7c1397ba670288306cf14ae632567c13d
RitikaBhandari07 <167596764+RitikaBhandari07@users.noreply.github.com>
2024-05-06 23:44:35 +0530

    Add files via upload

---
+++ loops2.py
@@ -0,0 +1,15 @@
+# start=int(input("enter your starting number"))
+# end=int(input("enter your end number"))
+# step=int(input("enter your step size"))
+# for i in range(start,end,step):
+#     print(i)
+
+
+# start=2
+# while(start<=20):
+#     print(start)
+#     start=start+2
+
+
+for  i in[2,20,1]:
+    pass
