6c32e0c09a5e03ca09c971df670b9dc61b8bb44b
RitikaBhandari07 <167596764+RitikaBhandari07@users.noreply.github.com>
2024-05-07 22:35:55 +0530

    Add files via upload

---
+++ tupple1.py
@@ -0,0 +1,15 @@
+mylist=("ritika","prashant","bhandari","sonali","naitik")
+print(type(mylist))
+print(len(mylist))
+print(mylist[1])
+print(mylist[-1])
+print(mylist[2:4])
+print(mylist[:2])
+print(mylist[2:])
+print(mylist[-3:-1])
+
+
+if "sonali"in mylist:
+    print("yes sonali is there in our list")
+else:
+    print("no sonali is not there in our list")
