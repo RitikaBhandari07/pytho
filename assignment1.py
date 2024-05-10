0609cb2507b82cad10bfd7965aaf29f8995350ae
RitikaBhandari07 <167596764+RitikaBhandari07@users.noreply.github.com>
2024-05-10 22:17:42 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,17 @@
+vehicle=["splendor","activa","jupiter","pulsor"]
+print(vehicle)
+# remove activa from the list
+vehicle.remove("activa")
+print(vehicle)
+
+
+# add pulsor 220f in the list
+vehicle.append("pulsor 220f")
+print(vehicle)
+
+# replace splendor by yahama
+vehicle[0]='yamaha'
+print(vehicle)
+
+vehicle[2]="jupitor 2023"
+print(vehicle)
